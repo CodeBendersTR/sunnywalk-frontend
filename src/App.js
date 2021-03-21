f0de762a1bd2571dcca0d64bd5a956823f2ff1be
Marius <mariusgrygore@gmail.com>
2021-03-21 21:52:17 +0000

    Initialized the React project and created a basic structure + routing

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {Home, Login, Main, NotFound, Profile, Register} from './pages';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/'><Main/></Route>
+          <Route exact path='/login'><Login/></Route>
+          <Route exact path='/register'><Register/></Route>
+          <Route exact path='/home'><Home/></Route>
+          <Route exact path='/profile'><Profile/></Route>
+
+          {/* The last routing is for "Not found"; new pages must be added before this line*/}
+          <Route><NotFound/></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home';
+import Layout from './Layout';
+import Login from './Login';
+import Main from './Main';
+import NotFound from './NotFound';
+import Profile from './Profile';
+import Register from './Register';
+
+export { Home, Layout, Login, Main, NotFound, Profile, Register };
