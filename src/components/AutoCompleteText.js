1b9c6c680ec9cea01f109717c2e62e9a23452e4f
Ciaran <ShyguyCC@users.noreply.github.com>
2021-03-24 13:00:50 +0000

    Filtering of text list complete

--- src/components/AutoCompleteText.js
+++ src/components/AutoCompleteText.js
@@ -9,15 +9,41 @@ export default class AutoCompleteText extends React.Component{
             'Sara',
             'Jame',
         ];
+        //Javascript object. When created have a empty suggestions array.
+        this.state = {
+            suggestions: [],
+        };
     }
-
+    //when the text in the box has changed get its value 
+    onTextChanged = (e) =>{
+        const value = e.target.value;
+        let suggestions = [];
+        if(value.length > 0){
+            //Case insensitive regex to match what the user has entered to our array.
+            const regex = new RegExp(`^${value}`, 'i');
+            //update suggestions by using a sorted filter .
+            suggestions = this.items.sort().filter(v => regex.test(v));
+        }
+        //return updated suggestions.
+        this.setState(()=> ({suggestions}));
+    }
+    renderSuggestions(){
+        const{suggestions} = this.state;
+        //if theres no input show nothing.
+        if(suggestions.length === 0){
+            return null
+        }
+        return(
+            <ul>
+                {suggestions.map((item) => <li>{item}</li>)}
+            </ul>
+        )
+    }
     render(){
         return(
             <div>
-                <input type="text" />
-                <ul>
-                    {this.items.map((item) => <li>{item}</li>)}
-                </ul>
+                <input onChange={this.onTextChanged} type="text" />
+                {this.renderSuggestions()}
             </div>
         )
     }
