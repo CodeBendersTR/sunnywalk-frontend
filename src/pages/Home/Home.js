e7106ab816986d6a6634094897f5ebaeb01769d9
Ranjini <ranj.jini@gmail.com>
2021-03-24 22:31:53 +0000

    Created a static User Home page with minimum required features

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,45 @@
 import "./Home.css";
 import { Menu } from "..";
+import {Link} from "react-router-dom";
+//import Button from 'muicss/lib/react/button';
+
+//const logo = require('/public/logo.jpg');
 
 function Home() {
     return (
         <Menu>
-            <h1>This is the Home Page</h1>
+            <header>
+              <h1 className="text_align">User Home</h1>
+              <h3>Welcome "User Name"</h3>
+              <h4>Today's best times to take a walk</h4>
+            </header>
+            <main>
+              <div>
+                <ul>
+                  <li>
+                    <p></p>
+                    <displaytime>00:00:00</displaytime>
+                    <button className="notify-user">Notify</button>
+                    <button>Log Feedback</button>
+                  </li>
+                  <li>
+                    <p></p>
+                    <displaytime>00:00:00</displaytime>
+                    <button>Notify</button>
+                    <button>Log Feedback</button>
+                  </li>
+                  <li>
+                    <p></p>
+                    <displaytime>00:00:00</displaytime>
+                    <button>Notify</button>
+                    <button>Log Feedback</button>
+                  </li>
+                </ul>
+              </div>
+            </main>
+            <Link exact to="/main">
+              Logout
+            </Link>
         </Menu>
     );
 }
