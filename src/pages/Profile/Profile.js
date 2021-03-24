91845496afe36296e37c1fabca3d07668ba6f791
Ciaran <ShyguyCC@users.noreply.github.com>
2021-03-24 11:56:24 +0000

    Updated Profile page and starting on a auto complete text box.

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,12 +1,42 @@
 import "./Profile.css";
 import { Menu } from "..";
+import React from "react";
 
-function Profile() {
-    return (
-        <Menu>
-            <h1>This is the Profile Page</h1>
-        </Menu>
-    );
+export default class Profile extends React.Component{
+    constructor(props){
+            super(props);
+            this.state = {
+                    isHidden: false,
+            }
+    }
+
+    toggleIsHidden(){
+            this.setState((currentState) => ({
+                    isHidden: !currentState.isHidden, 
+            }));
+    }
+    render(){
+            return(        
+            <Menu>
+                    <h1>This is the Profile Page</h1>
+                    
+            <ul>
+                    <li>Welcome to your edit profile page, please fill in and confirm any edits you would like to do.</li>
+            </ul>
+            <ul>User name</ul> 
+            <ul>Location</ul>
+            <ul>notification settings</ul>
+            <ul>Automatically update walks</ul>
+            <ul>Weather preference</ul>
+            <ul>Route type</ul>
+
+            <button onClick={()=>this.toggleIsHidden()}>toggle</button>
+            {this.state.isHidden && this.props.text}
+            
+    
+            </Menu>
+    
+            );
+    }
 }
 
-export default Profile;
