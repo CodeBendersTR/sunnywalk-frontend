746955e3d76e2af5ae56c67de576ceec9b7fe82c
Ciaran <ShyguyCC@users.noreply.github.com>
2021-03-25 15:22:44 +0000

    Added Dropdown menus. ToDo have a way to save the info from the drop down menu.

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,9 @@
 import "./Profile.css";
 import { Menu } from "..";
-import React from "react";
+import React, {useState} from "react";
 import {AutoCompleteText, UkCities} from '../../components';
+import {Select, MenuItem, makeStyles} from "@material-ui/core"
+
 
 export default class Profile extends React.Component{
     constructor(props){
@@ -17,6 +19,8 @@ export default class Profile extends React.Component{
             }));
     }
     render(){
+const [value,setValue] = useState("");
+const handleChange = e => setValue(e.target.value);
             return(        
             <Menu>
                 <h1>This is the Profile Page</h1>
@@ -24,13 +28,32 @@ export default class Profile extends React.Component{
                     <li>Welcome to your edit profile page, please fill in and confirm any edits you would like to do.</li>
                 </ul>
                 <ul>User name <input></input></ul> 
-                <ul>Location 
+                <ul>Change Password <input></input></ul> 
+                <ul>Preferred location 
                         <AutoCompleteText items={UkCities}/>
                 </ul>
-                <ul>notification settings</ul>
-                <ul>Automatically update walks</ul>
-                <ul>Weather preference</ul>
-                <ul>Route type</ul>
+                <ul>notification settings  
+                        <Select onChange={this.handleChange}>
+                                <MenuItem value={"Email"}>Email</MenuItem>
+                                <MenuItem value={"Web Notification"}>Web notification</MenuItem>
+                        </Select>
+                </ul>
+                <ul>Automatically update walks
+                        <Select>
+                                <MenuItem value={"Yes"}>Yes</MenuItem>
+                                <MenuItem value={"No"}>No</MenuItem>
+                        </Select>
+                </ul>
+                <ul>Weather preference
+                        <Select>
+                                <MenuItem value={"Sunny"}>Sunny</MenuItem>
+                                <MenuItem value={"Cloudy"}>Cloudy</MenuItem>
+                                <MenuItem value={"Raining"}>Raining</MenuItem>
+                                <MenuItem value={"Snow"}>Snow</MenuItem>
+                                <MenuItem value={"Windy"}>Windy</MenuItem>
+                        </Select>
+                </ul>
+                <p>you have secected </p>
                 {this.state.isHidden && this.props.text}
                 
             </Menu>
