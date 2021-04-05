fc636a6c3313026d30fa6804d899047a257cba81
Ciaran <ShyguyCC@users.noreply.github.com>
2021-04-05 13:22:52 +0100

    Created very simple tests for multiselect components.

---
+++ src/components/MultiSelect/MultiSelect.test.js
@@ -0,0 +1,21 @@
+
+import {render, screen } from '@testing-library/react'
+import {
+    WeatherPreferenceSelect,
+    NotificationPreferenceSelect,
+} from "../../components";
+import '@testing-library/jest-dom/extend-expect';
+
+
+test('Check weather select main text shows', ()=>{
+    render(<WeatherPreferenceSelect />);
+    const input = screen.getByText('Weather');
+    expect(input).toBeInTheDocument();
+});
+
+
+test('Check notification select main text shows', ()=>{
+    render(<NotificationPreferenceSelect />);
+    const input = screen.getByText('Notification');
+    expect(input).toBeInTheDocument();
+});
