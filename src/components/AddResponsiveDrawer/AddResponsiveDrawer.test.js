19e1013cae691fbfc9e005c401d901b08e280fa2
Ciaran <ShyguyCC@users.noreply.github.com>
2021-04-09 15:11:13 +0100

    Check Welcome username renders change

--- src/components/AddResponsiveDrawer/AddResponsiveDrawer.test.js
+++ src/components/AddResponsiveDrawer/AddResponsiveDrawer.test.js
@@ -1,14 +1,14 @@
-/*
 import { render, screen } from "@testing-library/react";
 import { AddResponsiveDrawer } from "../../components";
 import "@testing-library/jest-dom/extend-expect";
 
 
-test("Check the login text renders", () => {
+test("Check that Welcome UserName Renders", () => {
     render(<AddResponsiveDrawer />);
-    const input = screen.getByText("Already have an account? Log In");
+    const input = screen.getByText("Welcome UserName");
     expect(input).toBeInTheDocument();
 });
+/*
 To Do:
 Find what "Invariant failed: You should not use <Link> outside a <Router>" means here.
 */
