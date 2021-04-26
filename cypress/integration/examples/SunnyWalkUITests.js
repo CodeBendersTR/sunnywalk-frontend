8c0be1b5a8b01bc97252bb035dc6fe805f3e6b43
Ciaran <ShyguyCC@users.noreply.github.com>
2021-04-26 16:12:05 +0100

    Added tests for the select menu in profile.

--- cypress/integration/examples/SunnyWalkUITests.js
+++ cypress/integration/examples/SunnyWalkUITests.js
@@ -76,4 +76,18 @@ context('Actions', () => {
     cy.get('.MuiInputBase-input').eq(1).type('FakePassword').should('have.value', 'FakePassword')
     cy.get('.MuiInputBase-input').eq(2).type('Walthamstow').should('have.value', 'Walthamstow')
   })
+  it('Check the select button for walker types have values assigned to them.', () => {
+    cy.contains('Login').click()
+    cy.contains('LogIn').click()
+    cy.contains('Profile').click()
+    
+    cy.get('.MuiSelect-select').eq(0).click()
+    cy.get('.MuiListItem-root').eq(6).contains('Active wheelchair')
+    cy.get('.MuiListItem-root').eq(7).contains('Electronic wheelchair')
+    cy.get('.MuiListItem-root').eq(8).contains('Pram')
+    cy.get('.MuiListItem-root').eq(9).contains('Push walker')
+    cy.get('.MuiListItem-root').eq(10).contains('Scooter')
+    cy.get('.MuiListItem-root').eq(11).contains('Skater/Rollerblades')
+
+  })
 })
