5a9591eb04ae07df1a4e637870d3e77a15270cb9
Ciaran <ShyguyCC@users.noreply.github.com>
2021-04-27 14:43:37 +0100

    Added comments for to make it easier to read. Added seveal tests including testing if links on the front page work and the select menus on edit profile work.

--- cypress/integration/TestsFolder/SunnyWalkUITests.js
+++ cypress/integration/TestsFolder/SunnyWalkUITests.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
-
-context('Actions', () => {
+//This is what happens before any test. as we are testing a local build of the site we first visit localhost:3000
+context('Starting', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
   })
+  //The following 8 tests are testing that links work with the buttons.
   it('Check if login button goes to login page.', () => {
     cy.contains('Login').click()
     cy.url().should('include', '/Login')
@@ -49,12 +50,14 @@ context('Actions', () => {
     cy.contains('Profile').click()
     cy.url().should('include', '/Profile')
   })
+  //check that a button is visible if if the user has been logged out.
   it('When logged in and click logout check if user can log back in.', () => {
     cy.contains('Login').click()
     cy.contains('LogIn').click()
     cy.contains('Logout').click()
     cy.contains('Login').should('be.visible')
   })
+  //these two tests are checking the accessability of going from the login page to the rester page for the forgetful.
   it('Check i can go from logging in to register page if i realise i do not have a account.', () => {
     cy.contains('Login').click()
     cy.contains('No account yet? Register').click()
@@ -65,9 +68,16 @@ context('Actions', () => {
     cy.contains('Already have an account? Login').click()
     cy.url().should('include', '/Login')
   })
+  //check the link to the github page works. unfortuntly cypress doesnt allow for testing for websites that use other url ie going from google to facebook is not allowed all testing needs to be done in one space.
   it('If i click the sunny walk logo and text i should go into the development github.', () => {
     cy.get('a').should('have.attr','href', 'https://github.com/CodeBendersTR')
   })
+  it('Check if all links under embrace the power of the sun go to the right places.', () => {
+    cy.get('a').eq(3).should('have.attr','href','https://www.webmd.com/balance/ss/slideshow-health-benefits-nature')
+    cy.get('a').eq(4).should('have.attr','href','https://www.webmd.com/balance/ss/slideshow-health-benefits-nature')
+    cy.get('a').eq(5).should('have.attr','href','https://www.webmd.com/food-recipes/features/vitamin-d-vital-role-in-your-health')
+  })
+  //test that the typing in editing profile page works.
   it('Check the text boxes in the profile button works.', () => {
     cy.contains('Login').click()
     cy.contains('LogIn').click()
@@ -76,11 +86,11 @@ context('Actions', () => {
     cy.get('.MuiInputBase-input').eq(1).type('FakePassword').should('have.value', 'FakePassword')
     cy.get('.MuiInputBase-input').eq(2).type('Walthamstow').should('have.value', 'Walthamstow')
   })
+  //Next three tests check the values that are in the select boxes within the the edit profile page.
   it('Check the select button for walker types have values assigned to them.', () => {
     cy.contains('Login').click()
     cy.contains('LogIn').click()
     cy.contains('Profile').click()
-    
     cy.get('.MuiSelect-select').eq(0).click()
     cy.get('.MuiListItem-root').eq(6).contains('Active wheelchair')
     cy.get('.MuiListItem-root').eq(7).contains('Electronic wheelchair')
@@ -90,4 +100,32 @@ context('Actions', () => {
     cy.get('.MuiListItem-root').eq(11).contains('Skater/Rollerblades')
 
   })
+  it('Check the select button for walker types have values assigned to them.', () => {
+    cy.contains('Login').click()
+    cy.contains('LogIn').click()
+    cy.contains('Profile').click()
+    cy.get('.MuiSelect-select').eq(1).click()
+    cy.get('.MuiListItem-root').eq(6).contains('Email')
+    cy.get('.MuiListItem-root').eq(7).contains('Web Notification')
+  })
+
+  it('Check the select button for walker types have values assigned to them.', () => {
+    cy.contains('Login').click()
+    cy.contains('LogIn').click()
+    cy.contains('Profile').click()
+    cy.get('.MuiSelect-select').eq(2).click()
+    cy.get('.MuiListItem-root').eq(6).contains('Sunny')
+    cy.get('.MuiListItem-root').eq(7).contains('Partly Sunny')
+    cy.get('.MuiListItem-root').eq(8).contains('Cloudy')
+    cy.get('.MuiListItem-root').eq(9).contains('Raining')
+    cy.get('.MuiListItem-root').eq(10).contains('Snow')
+    cy.get('.MuiListItem-root').eq(11).contains('Windy')
+
+  })
+//this only checks if one image is colorblind friendly as other images on the front page are ImageLoad and therefor hides its accessibility.
+  it('Check the images are colorblind friendly.', () => {
+    cy.get('img').eq(1).should('have.attr','accessibility', 'colorblind')
+  })
+  //
+
 })
