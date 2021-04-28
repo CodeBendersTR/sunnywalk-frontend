345a85b816d6ef305a6ecfec6ea19f5c08b1f384
Susan Luebke <78167908+SusanLuebke@users.noreply.github.com>
2021-04-28 19:00:57 +0100

    [F3033] Prettify clean up and logo now links to main

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,36 +1,37 @@
 import React from "react";
 import "./Home.css";
 //import { Menu } from "..";
-import { AddResponsiveDrawer, UniversalHeader, SimpleRating } from "../../components";
-
+import { AddResponsiveDrawer, UniversalHeader } from "../../components";
 
 function Home() {
     return (
         <div>
             <div class="parent">
-                <a href="/../images/UVIGuide.pdf" download rel="noopener noreferrer" target="_blank">
-                    Download File                  
-                    <img
-                            class="image1"
-                            src="images/UVIndex.jpg"
-                            alt="UV Index ratings"
-
-
-                        ></img>
+                <a
+                    href="/../images/UVIGuide.pdf"
+                    download
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    Download File
+                    <img class="image1" src="images/UVIndex.jpg" alt="UV Index ratings"></img>
                 </a>
-            </div>          
+            </div>
             <UniversalHeader
-                // ButtonMain="Main"
-                // ButtonLogin="Login"
-                // ButtonRegister="Register"
-                // ButtonProfile="Main" FirstButton={"../Main"}
-                // ButtonUserHome="Profile" SecondButton={"../Profile"}
+            // ButtonMain="Main"
+            // ButtonLogin="Login"
+            // ButtonRegister="Register"
+            // ButtonProfile="Main" FirstButton={"../Main"}
+            // ButtonUserHome="Profile" SecondButton={"../Profile"}
             />
-            <AddResponsiveDrawer Greeting="User Home Page" ButtonProfile="Main" FirstButton={"../"}
-                                 ButtonUserHome="Profile" SecondButton={"../Profile"}/>                                
-
+            <AddResponsiveDrawer
+                Greeting="User Home Page"
+                ButtonProfile="Main"
+                FirstButton={"../"}
+                ButtonUserHome="Profile"
+                SecondButton={"../Profile"}
+            />
         </div>
-
     );
 }
 
