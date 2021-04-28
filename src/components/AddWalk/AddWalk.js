30471d7104d11a385726b7a0d88deee9540a2f5e
Marius Grigore <mariusgrygore@gmail.com>
2021-04-28 15:56:04 +0100

    Implemented the link between AddCard component and backend

--- src/components/AddWalk/AddWalk.js
+++ src/components/AddWalk/AddWalk.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { WbSunny } from '@material-ui/icons';
 import { Rating } from '@material-ui/lab';
@@ -11,6 +11,8 @@ import {
   Typography,
   Box,
 } from '@material-ui/core';
+import axios from 'axios';
+import getConfig from "../../modules/Config";
 
 const useStyles = makeStyles({
   root: {
@@ -30,6 +32,44 @@ const useStyles = makeStyles({
 });
 
 function AddWalk(props) {
+
+  const [rating, setRating] = useState(0);
+  const [addWalkResponse, setAddWalkResponse] = useState([]);
+  const [addWalkStatus, setAddWalkStatus] = useState("waiting");
+
+  function handleSubmit() {
+    const timeString = document.getElementById("addWalkTime").value;
+    const time = new Date(timeString).getTime() / 1000;
+
+    const location = document.getElementById("addWalkLocation").value;
+    const locationMatch = /(?<name>.+) \((?<lat>.+),(?<lon>.+)\)/.exec(location);
+    const locationName = locationMatch.groups.name;
+    const latitude = locationMatch.groups.lat;
+    const longitude = locationMatch.groups.lon;
+
+    const addWalkDto = {
+      "time": time,
+      "locationName": locationName,
+      "lat": latitude,
+      "lon": longitude,
+      "rating": rating
+    };
+    
+    let registerPromise = axios.post(getConfig("backend-url") + "/walk/add/1", addWalkDto);
+      setAddWalkStatus("loading");
+        registerPromise.then(
+            (response) => {
+                setAddWalkResponse(response);
+                setAddWalkStatus("fulfilled");
+            }
+        ).catch(
+            (err) => {
+                setAddWalkResponse(err.response);
+                setAddWalkStatus("error");
+            }
+        );
+  }
+
   const classes = useStyles();
   const date = new Date().toISOString().match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/g); // remove seconds and milliseconds
   return (
@@ -56,8 +96,8 @@ function AddWalk(props) {
             margin="normal"
             name="location"
             id="addWalkLocation"
-            label={props.location.lat + "|" + props.location.lng}
-            value={props.location.name}
+            label="Location"
+            value={props.location.name + " (" + props.location.lat + "," + props.location.lng + ")"}
             defaultValue="(Click on the map for location)"
             fullWidth
             required
@@ -65,11 +105,11 @@ function AddWalk(props) {
           />
         </div>
         <Box mb={1} mt={3} borderColor="transparent">
-          <Rating icon={<WbSunny fontSize="inherit" />} />
+          <Rating id="addWalkRating" icon={<WbSunny fontSize="inherit" />} onChange={(event, newValue) => {setRating(newValue)}} />
         </Box>
       </CardContent>
       <CardActions className={classes.actions}>
-        <Button size="medium">Add Walk</Button>
+        <Button size="medium" onClick={handleSubmit}>Add Walk</Button>
       </CardActions>
     </Card>
   );
