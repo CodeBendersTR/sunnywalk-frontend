ecc335c8337c146fbeaffcb3b2d6b5e26368cc59
Ciaran <ShyguyCC@users.noreply.github.com>
2021-05-01 10:01:57 +0100

    Created tests for addwalk component. Created one more test for Carddisplay. Created a test for universaldrawer. Going to do tests for card home component now.

--- src/components/CardDisplay/CardDisplay.test.js
+++ src/components/CardDisplay/CardDisplay.test.js
@@ -19,3 +19,15 @@ test("Check Rate Your Walks text renders.", () => {
     const input = screen.getByText("Rate Your Walks");
     expect(input).toBeInTheDocument();
 });
+
+test("Check walking on sunshine renders text renders.", () => {
+    render(<CardDisplay />);
+    const input = screen.getByText("Walking on sunshine...map your preferences to hit favourite spots whether a playground, dog or skate park or something completely new.");
+    expect(input).toBeInTheDocument();
+});
+
+test("Check song text renders.", () => {
+    render(<CardDisplay />);
+    const input = screen.getByText("Was your walk \"You are my sunshine\" or more like \"Ain't no sunshine\"...rate your walks and repeat the great ones...again and again.");
+    expect(input).toBeInTheDocument();
+});
